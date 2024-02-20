adf549d924e708394830aaab204b859910aae5e2
Адитя <75035219+pegvin@users.noreply.github.com>
2024-02-20 15:56:54 +0530

    Update sfd.h

--- src/sfd.h
+++ src/sfd.h
@@ -7,6 +7,7 @@
 
 #ifndef SFD_H
 #define SFD_H
+#pragma once
 
 #define SFD_VERSION "0.1.0"
 
@@ -20,7 +21,15 @@ typedef struct {
   int allowMultiple;
 } sfd_Options;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 const char* sfd_get_error(void);
 const char* sfd_open_dialog(sfd_Options* opt);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif // SFD_H
